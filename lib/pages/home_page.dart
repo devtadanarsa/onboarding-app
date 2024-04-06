d485f1e989ea08ef45f74495c9c9345ba7f6cb5b
Devtaaa <devtadanarsa69@gmail.com>
2024-04-06 19:45:28 +0800

    add bottom navigation bar in home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,8 +5,48 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text("Home Page"),
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      bottomNavigationBar: const BottomAppBar(
+        surfaceTintColor: Colors.white,
+        color: Colors.white,
+        child: Padding(
+          padding: EdgeInsets.only(left: 20, right: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.home_work_outlined),
+                  Text("Home"),
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.cases_outlined),
+                  Text("Interviews"),
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.mail_outline),
+                  Text("Messages"),
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.person_outline_sharp),
+                  Text("Account"),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
